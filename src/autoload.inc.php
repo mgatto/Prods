fd44421b8c624c8771963cc9669221160b1f2a8b
Colin Scott-Fleming <colinsf@arizona.edu>
2012-08-27 15:32:46 -0700

    Added spl_autoload_register() implementation of class loading - mostly for Drupal 7 library integration.

--- src/autoload.inc.php
+++ src/autoload.inc.php
@@ -16,6 +16,19 @@ function __autoload($className) {
 }
 
 /**
+ * autoload classes via spl_autoload_register()
+ *
+ * @uses classFolder()
+ * @param $className string
+ */
+spl_autoload_register(function ($className) {
+     $folder = classFolder($className);
+
+     if($folder)
+         require_once($folder.$className.".class.php");
+});
+
+/**
  * search for folders and subfolders with classes
  *
  * @param $className string
